1f0ea2416f77ce8096ecb6fd04f116341be79dfe
Nic Gordon <nic.gordon@inlight.com.au>
2019-03-16 13:25:16 +1100

    Move location permissions logic to new audit screen

--- src/screens/new-audit/index.js
+++ src/screens/new-audit/index.js
@@ -1,25 +1,65 @@
 import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 import { Button, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { Location, Permissions } from 'expo';
 import PropTypes from 'prop-types';
-import React, { Fragment } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 
 import auditActions from '../../state/actions/audit';
-import Auditor from '../../components/auditor';
 
-const NewAuditScreen = ({ actions, auditIsRunning, pingEndpoint }) => (
-  <View>
-    <Text>Audit screen boom!</Text>
-    {auditIsRunning ? (
-      <Fragment>
-        <Auditor pingEndpoint={pingEndpoint} />
-        <Button title="Stop" onPress={() => actions.audit.stopAudit()} />
-      </Fragment>
-    ) : (
-      <Button title="Start" onPress={() => actions.audit.startAudit()} />
-    )}
-  </View>
-);
+class NewAuditScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      locationPermissionGranted: false,
+      locationServicesEnabled: false,
+    };
+  }
+
+  componentWillMount() {
+    this.getLocationPermission();
+    this.getLocationStatus();
+  }
+
+  async getLocationPermission() {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === 'granted') {
+      this.setState({
+        locationPermissionGranted: true,
+      });
+    }
+  }
+
+  async getLocationStatus() {
+    const locationServicesEnabled = await Location.hasServicesEnabledAsync();
+    this.setState({
+      locationServicesEnabled,
+    });
+  }
+
+  render() {
+    const { actions, auditIsRunning } = this.props;
+    const { locationPermissionGranted, locationServicesEnabled } = this.state;
+
+    return (
+      <View>
+        <Text>Audit screen boom!</Text>
+        {!locationPermissionGranted && <Text>This function requires permission to access your location.</Text>}
+        {locationPermissionGranted && !locationServicesEnabled && (
+          <Text>Location services need to be enabled for this function to work.</Text>
+        )}
+        {auditIsRunning ? (
+          <Fragment>
+            <Button disabled={!locationPermissionGranted} title="Stop" onPress={() => actions.audit.stopAudit()} />
+          </Fragment>
+        ) : (
+          <Button title="Start" onPress={() => actions.audit.startAudit()} />
+        )}
+      </View>
+    );
+  }
+}
 
 NewAuditScreen.propTypes = {
   actions: PropTypes.shape({
@@ -29,7 +69,6 @@ NewAuditScreen.propTypes = {
     }).isRequired,
   }).isRequired,
   auditIsRunning: PropTypes.bool.isRequired,
-  pingEndpoint: PropTypes.string.isRequired,
 };
 
 NewAuditScreen.navigationOptions = {
